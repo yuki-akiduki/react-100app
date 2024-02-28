541df39af3ebc9c1aff180d7d189681b3791e1be
yuki-akiduki <yuki.akiduki1222@gmail.com>
2024-02-28 15:24:53 +0900

    fix：evn

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,16 +1,3 @@
-const Credentials = {
-  USERNAME: "user",
-  PASSWORD: "password",
-};
-
- // Verify credentials
-
- const creds = {
-  username: env.BASIC_USERNAME || Credentials.USERNAME,
-  password: env.BASIC_PASSWORD || Credentials.PASSWORD,
-};
-
-
 const errorHandler = async ({ next }) => {
   try {
     return await next();
@@ -19,12 +6,13 @@ const errorHandler = async ({ next }) => {
   }
 };
 
+const guardByBasicAuth = async ({ request, next, env }) => {
 
-const guardByBasicAuth = async ({ next, request, env}) => {
+
   // Check header
-  if (!request.headers.has("Authorization")) {
+  if (!request.headers.has('Authorization')) {
     return new Response(
-      "You need to login.",
+      'You need to login.',
       {
         status: 401,
         headers: {
@@ -32,38 +20,38 @@ const guardByBasicAuth = async ({ next, request, env}) => {
           'WWW-Authenticate': 'Basic realm="Input username and password"',
         },
       });
-  };
+  }
   // Decode header value
-  const [scheme, encoded] = request.headers.get("Authorization").split(' ');
+  const [scheme, encoded] = request.headers.get('Authorization').split(' ');
   if (!encoded || scheme !== 'Basic') {
     return new Response(
-      "Malformed authorization header.",
+      'Malformed authorization header.',
       {
         status: 400,
-      }
+      },
     );
   }
   const buffer = Uint8Array.from(atob(encoded), character => character.charCodeAt(0));
   const decoded = new TextDecoder().decode(buffer).normalize();
-  const index = decoded.indexOf(":");
+  const index = decoded.indexOf(':');
+  // eslint-disable-next-line no-control-regex
   if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
     return new Response(
-      "Invalid authorization value.",
+      'Invalid authorization value.',
       {
         status: 400,
-      }
+      },
     );
   }
 
-  // Verify credentials
   const username = decoded.substring(0, index);
   const password = decoded.substring(index + 1);
-  if (username !== creds.username || password !== creds.password) {
+  if (username !== env.BASIC_USERNAME || password !== env.BASIC_PASSWORD) {
     return new Response(
-      "Invalid username or password.",
+      'Invalid username or password.',
       {
         status: 401,
-      }
+      },
     );
   }
   return await next();
